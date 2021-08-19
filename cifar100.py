d0f384797178549fc3860a8bcafb2f16f7b5e4fd
AbdallahOmarAhmed <49597655+AbdallahOmarAhmed@users.noreply.github.com>
2021-08-19 16:42:34 +0200

    Update cifar100.py

--- cifar100.py
+++ cifar100.py
@@ -29,26 +29,47 @@ class cifar100Net(nn.Module):
         self.conv1 = nn.Conv2d(3,20,3)
         self.relu = nn.ReLU()
         self.pool = nn.MaxPool2d(2,2)
+        self.norm1 = nn.BatchNorm2d(20)
+        self.norm2 = nn.BatchNorm2d(50)
+        self.norm3 = nn.BatchNorm2d(100)
+        self.poolSame = nn.MaxPool2d(2,1)
         self.conv2 = nn.Conv2d(20,50,4)
-        self.fc1 = nn.Linear(50*6*6,200)
-        self.fc2 = nn.Linear(200,100)
+        self.conv3 = nn.Conv2d(50,100,3)
+        self.fc1 = nn.Linear(100*2*2,200)
+        self.fc2 = nn.Linear(200,150)
+        self.fc3 = nn.Linear(150,100)
+        self.dropout = nn.Dropout(0.25)
         # self.layer2 = torch.nn.ModuleList(self.layer2)
 
     def forward(self, x):
         out = self.conv1(x)
         out = self.relu(out)
         out = self.pool(out)
+        out = self.norm1(out)
+        out = self.dropout(out)
+
         out = self.conv2(out)
         out = self.relu(out)
         out = self.pool(out)
-        out = out.view(-1,50*6*6)
+        out = self.norm2(out)
+        out = self.dropout(out)
+
+        out = self.conv3(out)
+        out = self.relu(out)
+        out = self.pool(out)
+        out = self.norm3(out)
+        out = self.dropout(out)
+        
+        out = out.view(-1,100*2*2)
         out = self.fc1(out)
         out = self.fc2(out)
+        out = self.fc3(out)
         return out
 
 model = cifar100Net().to(device)
 Loss = nn.CrossEntropyLoss()
 optimaizer = torch.optim.Adam(model.parameters(), lr =learning_rate)
+scheduler = torch.optim.lr_scheduler.StepLR(optimaizer, step_size=20, gamma=0.8)
 
 # train loop
 for epoch in range(num_epochs):
@@ -63,7 +84,7 @@ for epoch in range(num_epochs):
         optimaizer.zero_grad()
         loss.backward()
         optimaizer.step()
-
+    scheduler.step()
     print(epoch + 1, '/', num_epochs, 'loss = ', loss.item())
     model.eval()
     # accuracy
